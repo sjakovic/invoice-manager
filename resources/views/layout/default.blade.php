ff890151638436e983207400b12c7310324ff782
Simo Jakovic <s.jakovic@gmail.com>
2022-04-13 16:43:45 +0200

    Adding feather icons

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -31,7 +31,6 @@
 @include('layout.base.translations')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
 <script src="/js/app.js"></script>
 @include('layout.base.page-scripts')
 </body>
